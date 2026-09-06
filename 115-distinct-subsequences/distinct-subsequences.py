6c8c8c22aceec80dcf650c5fa56fa3952aad4f8f
Jyoti sharma <jyotisharma4213@gmail.com>
2026-09-06 09:48:41 +0530

    Time: 415 ms (53.28%) | Memory: 75.6 MB (48.64%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -7,7 +7,7 @@ class Solution:
 
         dp = [[0] * (n + 1) for _ in range(m + 1)]
 
-        # Empty t can be formed in exactly 1 way
+        
         for i in range(m + 1):
             dp[i][0] = 1
 
@@ -15,11 +15,11 @@ class Solution:
             for j in range(1, n + 1):
 
                 if s[i - 1] == t[j - 1]:
-                    # Use this character OR skip it
+                    
                     dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
 
                 else:
-                    # Cannot use this character
+                
                     dp[i][j] = dp[i - 1][j]
 
         return dp[m][n]
